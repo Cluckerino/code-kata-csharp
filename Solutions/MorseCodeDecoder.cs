446291062ba161048b4efcb67c6d80fbfd32fa71
Cluckerino <elitedragonslayer@gmail.com>
2018-11-19 07:07:21 -0800

    stub + tests

---
+++ Solutions/MorseCodeDecoder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+/// <summary>
+/// Solution for the morse code kata.
+/// See https://www.codewars.com/kata/decode-the-morse-code/
+/// </summary>
+namespace Solutions
+{
+    /// <summary>
+    /// Class for the solution.
+    /// </summary>
+    public class MorseCodeDecoder
+    {
+        /// <summary>
+        /// Decodes the given morse code.
+        /// </summary>
+        /// <param name="morseCode"></param>
+        /// <returns></returns>
+        public static string Decode(string morseCode)
+        {
+            throw new NotImplementedException("Please provide some code.");
+        }
+    }
+}
---
+++ Tests/MorseCodeDecoderTest.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using Solutions;
+
+namespace Tests
+{
+    [TestFixture]
+    public class MorseCodeDecoderTest
+    {
+        /// <summary>
+        /// Example from Kata.
+        /// </summary>
+        [Test]
+        public void Decode_RegularString_CorrectResult()
+        {
+            string input = ".... . -.--   .--- ..- -.. .";
+            string expected = "HEY JUDE";
+
+            string actual = MorseCodeDecoder.Decode(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
