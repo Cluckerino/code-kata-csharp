c3aae76823a949d201a6f3e86d35c5f6692abb3d
Cluckerino <elitedragonslayer@gmail.com>
2018-11-22 13:40:12 -0800

    Added tests with uneven messages

--- Tests/ScytaleTest.cs
+++ Tests/ScytaleTest.cs
@@ -34,5 +34,29 @@ namespace Tests
 
             Assert.That(actual, Is.EqualTo(expected));
         }
+
+        /// <summary>
+        /// Encode a message that won't fit evenly into the cylinder.
+        /// </summary>
+        [Test]
+        public void Scytale_EncodeUneven()
+        {
+            var message = "CodeWars Scytale Kata";
+            var expected = "CW t aoaSaK drcla esyet";
+            var actual = Scytale.Encode(message, 6);
+            Assert.AreEqual(expected, actual);
+        }
+
+        /// <summary>
+        /// Decode a message that won't fit evDely into the cylinder.
+        /// </summary>
+        [Test]
+        public void Scytale_DecodeUneven()
+        {
+            var message = "CW t aoaSaK drcla esyet";
+            var expected = "CodeWars Scytale Kata";
+            var actual = Scytale.Decode(message, 6);
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
