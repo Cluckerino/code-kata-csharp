a8318c975cbd14a893a267fca85b70c02ea97373
Don Buenaventura <cluckerino@DonsMacNCheese.hsd1.or.comcast.net>
2018-11-22 11:31:32 -0800

    Stub + tests

---
+++ Solutions/Scytale.cs
@@ -0,0 +1,33 @@
+﻿namespace Solutions
+{
+    /// <summary>
+    /// Solution for the scytale Kata
+    /// https://www.codewars.com/kata/scytale-encoder-slash-decoder-ancient-spartans-cipher/csharp
+    /// </summary>
+    public static class Scytale
+    {
+        /// <summary>
+        /// Decodes a coded message using a scytale with the given number of sides.
+        /// </summary>
+        /// <param name="message">The coded message to decode.</param>
+        /// <param name="cylinderSides">Number of sides for the decoder.</param>
+        /// <returns>The decoded message.</returns>
+        public static string Decode(string message, int cylinderSides)
+        {
+            // TODO: implement me
+            return message;
+        }
+
+        /// <summary>
+        /// Encodes a plain message using a scytale with the given number of sides.
+        /// </summary>
+        /// <param name="message">The plain message to encode.</param>
+        /// <param name="cylinderSides">Number of sides for the encoder.</param>
+        /// <returns>The encoded message.</returns>
+        public static string Encode(string message, int cylinderSides)
+        {
+            // TODO: implement me
+            return message;
+        }
+    }
+}
---
+++ Tests/ScytaleTest.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using Solutions;
+
+namespace Tests
+{
+    /// <summary>
+    /// Scytale test class
+    /// </summary>
+    [TestFixture]
+    public class ScytaleTest
+    {
+        /// <summary>
+        /// Example encode test from Kata.
+        /// </summary>
+        [Test]
+        public void BasicEncodeTest()
+        {
+            var message = "HELPMEIAMUNDERATTACK";
+            var expected = "HENTEIDTLAEAPMRCMUAK";
+            var actual = Scytale.Encode(message, 4);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        /// <summary>
+        /// Example decode test from Kata.
+        /// </summary>
+        [Test]
+        public void BasicDecodeTest()
+        {
+            var message = "HENTEIDTLAEAPMRCMUAK";
+            var expected = "HELPMEIAMUNDERATTACK";
+            var actual = Scytale.Decode(message, 4);
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
