1a761838c315f63379f5f34c45978f944122c73c
Cluckerino <elitedragonslayer@gmail.com>
2018-11-22 14:14:12 -0800

    Initial solution

--- Solutions/Scytale.cs
+++ Solutions/Scytale.cs
@@ -1,4 +1,8 @@
-﻿namespace Solutions
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Solutions
 {
     /// <summary>
     /// Solution for the scytale Kata
@@ -14,8 +18,28 @@
         /// <returns>The decoded message.</returns>
         public static string Decode(string message, int cylinderSides)
         {
-            // TODO: implement me
-            return message;
+            //! Example: Message has 21 letters for a 6 cylinder scytale.
+            //! Since 21 % 6 = 1 != 0, the message will need ceil(21 / 6) = 4 strips.
+            //! Only 21 % 6 = 1 of those strips will be filled with 6 letters, the rest will have 5.
+
+            // Calculate the number of strips.
+            // If remainderStrips == 0 all strips are full (i.e. the message divided fully into the cylinder).
+            // Otherwise, we need to add 1 since integer division does floor, and we need ceil.
+            var noOfStrips = Math.DivRem(message.Length, cylinderSides, out var remainderStrips);
+            if (remainderStrips > 0) noOfStrips++;
+
+            // Create jagged arrays for the rows.
+            var rows = Enumerable.Repeat(0, cylinderSides)
+                .Select(_ => new List<char>())
+                .ToList();
+
+            // Iterate throught the message and assign each letter to the appropriate row.
+            for (int i = 0; i < message.Length; i++)
+                rows[i % cylinderSides].Add(message[i]);
+
+            // Join rows to create the message. Trim the ends.
+            return string.Concat(rows.SelectMany(r => r))
+                .TrimEnd();
         }
 
         /// <summary>
@@ -26,8 +50,30 @@
         /// <returns>The encoded message.</returns>
         public static string Encode(string message, int cylinderSides)
         {
-            // TODO: implement me
-            return message;
+            // See Decode for explanation of counting strips.
+            var noOfStrips = Math.DivRem(message.Length, cylinderSides, out var remainderStrips);
+            if (remainderStrips > 0)
+            {
+                noOfStrips++;
+                // We also want to pad with spaces here so that it wraps correctly at the bottom row.
+                message = message.PadRight(noOfStrips * cylinderSides);
+            }
+
+            // Create fixed arrays for the strips.
+            var strips = Enumerable.Repeat(0, noOfStrips)
+                .Select(_ => new char[cylinderSides])
+                .ToList();
+
+            // Iterate throught the message and assign each letter to the appropriate location of each strip.
+            for (int i = 0; i < message.Length; i++)
+            {
+                var rowInd = Math.DivRem(i, noOfStrips, out var stripInd);
+                strips[stripInd][rowInd] = message[i];
+            }
+
+            // Join rows to create the message. Trim the ends.
+            return string.Concat(strips.Select(r => new string(r)))
+                .TrimEnd();
         }
     }
 }
