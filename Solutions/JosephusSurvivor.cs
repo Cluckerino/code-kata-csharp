607bd550ef2eed7722973cff6821b9320cc072d2
Cluckerino <elitedragonslayer@gmail.com>
2018-12-06 14:54:16 -0800

    Added stub + tests

---
+++ Solutions/JosephusSurvivor.cs
@@ -0,0 +1,25 @@
+using System;
+
+/// <summary>
+/// Solution Josephus survivor kata.
+/// See https://www.codewars.com/kata/josephus-survivor/csharp
+/// </summary>
+namespace Solutions
+{
+    /// <summary>
+    /// Class for the solution.
+    /// </summary>
+    public static class JosephusSurvivor
+    {
+        /// <summary>
+        /// Finds the survivor of a Josephus permutation.
+        /// </summary>
+        /// <param name="n">Initial number of people in the permutation.</param>
+        /// <param name="k">Steps until someone is eliminated.</param>
+        /// <returns>The position of the survivor.</returns>
+        public static int JosSurvivor(int n, int k)
+        {
+            return 0;
+        }
+    }
+}
