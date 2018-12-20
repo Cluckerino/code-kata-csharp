2b05ca8fc85adedbee90844c0c29d8253b2e9e9b
Cluckerino <elitedragonslayer@gmail.com>
2018-12-19 23:14:51 -0800

    Stub and test for the kata

---
+++ Solutions/Tribonacci.cs
@@ -0,0 +1,26 @@
+using System.Linq;
+
+/// <summary>
+/// Solution Josephus survivor kata.
+/// See https://www.codewars.com/kata/josephus-survivor/csharp
+/// </summary>
+namespace Solutions
+{
+    /// <summary>
+    /// Class for the solution.
+    /// </summary>
+    public partial class Xbonacci
+    {
+        /// <summary>
+        /// Given the first 3 terms (the signature) of the Tribonacci sequence, extend the sequence
+        ///  to the nth term (n includes the signature).
+        /// </summary>
+        /// <param name="signature">The first 3 terms of the sequence.</param>
+        /// <param name="n">The index of the last term, i.e. the length of the output.</param>
+        /// <returns>An array containing n terms of the Tribonacci sequence.</returns>
+        public double[] Tribonacci(double[] signature, int n)
+        {
+            return Enumerable.Empty<double>().ToArray();
+        }
+    }
+}
